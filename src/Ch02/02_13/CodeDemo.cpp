92f3d787210b016f64a8c7f5b48c345ba0d747ac
hy8899 <72721259+hy8899@users.noreply.github.com>
2024-05-23 07:48:14 +0000

    chapter 2

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -9,6 +9,7 @@ int main(){
     float result; 
 
     // Write your code here
+    result = (float)(nums[0] + nums[1] + nums[2] + nums[3] + nums[4]) / 5.0f;
     
     std::cout << "The average is " << result << std::endl;
     
